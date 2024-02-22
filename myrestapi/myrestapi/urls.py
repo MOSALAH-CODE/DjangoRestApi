b8f7e0a5dac58081df501ed2e2398b8363c25423
Mohammad Salah <m.salah@alifbee.com>
2024-02-22 20:58:04 +0300

    31. API Endpoints Overview

--- myrestapi/myrestapi/urls.py
+++ myrestapi/myrestapi/urls.py
@@ -22,6 +22,7 @@ from updates.views import (json_example, JsonCBV, JsonCBV2, SerilizedDetailView,
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/updates/", include("updates.api.urls")),
+    path("api/status/", include("status.api.urls")),
 
     # path('json/cbv', JsonCBV.as_view()),
     # path('json/cbv2', JsonCBV2.as_view()),
